4b58338743b9d9d6bf3bb2f4565a53a0a49026a5
Tea <tdinhla@protonmail.com>
2018-10-24 12:23:40 -0700

    add validations for Speaker model

--- app/models/speaker.rb
+++ app/models/speaker.rb
@@ -1,2 +1,5 @@
 class Speaker < ApplicationRecord
+  validates :first_name, :last_name, length: { minimum: 2 }
+  validates :email, uniqueness: true
+  validates :age, numericality: { greater_than_or_equal_to: 18 }
 end
